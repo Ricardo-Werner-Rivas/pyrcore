c35a86aea7fb3357ad945d2c58de07f041ba0d2f
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2025-11-20 15:26:51 +0100

    Add __init__ file to the package

---
+++ pyrcore/__init__.py
@@ -0,0 +1,14 @@
+"""
+Provides several R objects and functions.\n
+---
+## Implemented features
+* Class `Vector`: Implementation of R atomic vectors.
+* Combination function (`c()`): Implementation of R `c()` function.
+## Remaining intended features
+* Class `matrix`: Implementation of R matrixes and `matrix()` function.
+* Class `TimeSeries`: Implementation of R univariate time series.
+* Class `MultiVarTimeSeries`: Implementation of R multivariate time series.
+* Function `ts()`: Implementation of R `ts()` function.
+"""
+from .core import Vector
+from .functions import c
