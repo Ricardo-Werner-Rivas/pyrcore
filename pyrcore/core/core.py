534cab92ad27eea8bacc694a3fa782169766a09c
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2025-12-14 16:25:24 +0100

    Update documentation

--- pyrcore/core/core.py
+++ pyrcore/core/core.py
@@ -20,7 +20,7 @@ from abc import abstractmethod,ABCMeta
 class RObject(metaclass=ABCMeta):
     #& Missing code comments
     """
-    Base class for all R-based or *R-like* object or class in this package.
+    Base class for every R-based or *R-like* object or class in this package.
     """
     #* METHODS
     # __init__
