b76603c2499d8e82ff7afc56270d12d9cebc5d9b
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2026-01-16 12:53:08 +0100

    Add integer division arithmetic dunder method ('__floordiv__') to class 'matrix'

--- pyrcore/subclasses.py
+++ pyrcore/subclasses.py
@@ -337,4 +337,38 @@ class matrix(RObject,Generic[MT]):
             raise TypeError(f"Object type {data_type} is not operable with matrixes")
     
     # Integer division (floor division)
+    def __floordiv__(self,value):
+        if isinstance(value,matrix):
+            if self.dim==value.dim:
+                return matrix(c((self._data//value._data).flatten()),self.nrow,self.ncol,True,attributes=self.attributes)
+            else:
+                raise ArithmeticError("Dimensions are not compatible")
+        elif isinstance(value,Vector):
+            if len(value)>self.nrow*self.ncol:
+                raise ArithmeticError("More values in vector than in matrix")
+            elif len(value)==self.nrow*self.ncol:
+                return matrix(c(self._data.flatten()//value._data),self.nrow,self.ncol,True,attributes=self.attributes)
+            else:
+                result=self._data.copy()
+                pos=0
+                end=False
+                for j in range(self.ncol):
+                    for i in range(self.nrow):
+                        try:
+                            result[i,j]//=value[pos]
+                        except IndexError:
+                            end=True
+                            break
+                        pos+=1
+                    if end:
+                        break
+                return matrix(c(result.flatten()),self.nrow,self.ncol,True,attributes=self.attributes)
+        elif isinstance(value,(int,float)):
+            return matrix(c(self._data.flatten()//value),self.nrow,self.ncol,True,attributes=self.attributes)
+        else:
+            data_type=str(type(value))
+            data_type=data_type[data_type.find("'"):data_type.rfind("'")+1]
+            raise TypeError(f"Object type {data_type} is not operable with matrixes")
+    
+    # Module
     #! Missing
