d720f8660aa9eca8d64b94c4381ef6bd6c63689e
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2026-04-07 22:57:30 +0200

    Update package general documentation with development status

--- pyrcore/__init__.py
+++ pyrcore/__init__.py
@@ -4,8 +4,8 @@ Provides several R objects and functions.\n
 ## Implemented features
 * Class `Vector`: Implementation of R atomic vectors.
 * Combination function (`c()`): Implementation of R `c()` function.
-## Remaining intended features
 * Class `matrix`: Implementation of R matrixes and `matrix()` function.
+## Remaining intended features
 * Class `TimeSeries`: Implementation of R univariate time series.
 * Class `MultiVarTimeSeries`: Implementation of R multivariate time series.
 * Function `ts()`: Implementation of R `ts()` function.
