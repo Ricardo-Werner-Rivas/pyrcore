49e2b551a2ff45376f21802aad6219f57c185085
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2026-06-13 15:15:00 +0200

    Add `_aux_classes.py` file which contains the indexers used for `loc[]` and `iloc[]` properties

---
+++ pyrcore/_aux_classes.py
@@ -0,0 +1,156 @@
+# Imports
+from .core import Vector
+from .functions import c
+from .subclasses import MultiVariateTimeSeries
+from functools import singledispatchmethod
+from types import NoneType
+
+def _testSlice(index:slice,label:bool=True):
+    return True\
+    if\
+        isinstance(index.start,(str if label else int,Vector,NoneType))\
+        and isinstance(index.stop,(str if label else int,Vector,NoneType))\
+        and type(index.start)==type(index.stop)\
+    else False
+
+# Indexer for "loc" property
+class _LocIndexer:
+    def __init__(self,data:MultiVariateTimeSeries):
+        self.data=data
+    
+    @singledispatchmethod
+    def __getitem__(self,index:Vector[int]|str|slice|tuple[Vector[int]|str|slice,str|slice]):
+        raise TypeError
+    @__getitem__.register
+    def _(self,index:Vector):
+        return c(self.data._data[self.data._time.index(index)],names=self.data.colnames)
+    @__getitem__.register
+    def _(self,index:str):
+        return c(self.data._data[:,self.data.colnames.index(index)],names=self.data._time)
+    @__getitem__.register
+    def _(self,index:slice):
+        if _testSlice(index):
+            return MultiVariateTimeSeries(
+                self.data._data[
+                    :,
+                    slice(
+                        self.data.colnames.index(index.start),
+                        self.data.colnames.index(index.stop),
+                        index.step
+                    )
+                ]
+            )
+        else:
+            raise TypeError
+    @__getitem__.register
+    def _(self,index:tuple):
+        index=list(index)
+        for label in index:
+            if isinstance(label,str):
+                pass
+            elif isinstance(label,slice) and _testSlice(label):
+                index[index.index(label)]=slice(
+                    (self.data.rownames if index.index(label)==0 else self.data.colnames).index(label.start),
+                    (self.data.rownames if index.index(label)==0 else self.data.colnames).index(label.stop),
+                    label.step
+                )
+            else:
+                raise TypeError
+        index=tuple(index)
+        return self.data._data[index]
+    
+    def __setitem__(self,index,value):
+        self.__getitem__(index)
+        self.data._data[index]=value
+
+# Indexer for "iloc" property
+class _IlocIndexer:
+    def __init__(self,data:MultiVariateTimeSeries):
+        self.data=data
+    
+    @singledispatchmethod
+    def __getitem__(self,index:int|slice|tuple[int|slice,int|slice]):
+        raise TypeError
+    @__getitem__.register
+    def _(self,index:int):
+        return c(self.data._data[index],names=self.data.colnames)
+    @__getitem__.register
+    def _(self,index:slice):
+        if _testSlice(index,False):
+            return self.data._data[index]
+        else:
+            raise TypeError
+    @__getitem__.register
+    def _(self,index:tuple):
+        for pos in index:
+            if isinstance(pos,int):
+                pass
+            elif isinstance(pos,slice) and _testSlice(pos,False):
+                pass
+            else:
+                raise TypeError
+        return self.data._data[index]
+    
+    def __setitem__(self,index,value):
+        self.__getitem__(index)
+        self.data._data[index]=value
+
+#// class _LocIndexer:
+#//     def __init__(self,data:MultiVariateTimeSeries):
+#//         self.data=data
+    
+#//     @singledispatchmethod
+#//     def __getitem__(self,label:str|slice|tuple[str|slice,str|slice]):
+#//         raise TypeError
+#//     @__getitem__.register
+#//     def _(self,label:str):
+#//         return self.data[label]
+#//     @__getitem__.register
+#//     def _(self,label:slice):
+#//         if not all(tuple(isinstance(element,(str,NoneType)) for element in (label.start,label.stop))):
+#//             raise TypeError
+#//         else:
+#//             return self.data[label]
+#//     @__getitem__.register
+#//     def _(self,label:tuple[str|slice]):
+#//         if not all(tuple(isinstance(axis,(str,slice)) for axis in label)):
+#//             raise TypeError
+#//         else:
+#//             for element in label:
+#//                 if isinstance(element,slice) and not all((isinstance(element.start,(str,NoneType)),isinstance(element.stop,(str,NoneType)))):
+#//                     raise TypeError
+#//         return self.data[label]
+    
+#//     def __setitem__(self,index,value):
+#//         self.__getitem__(index)
+#//         self.data[index]=value
+
+#// class _IlocIndexer:
+#//     def __init__(self,data:MultiVariateTimeSeries):
+#//         self.data=data
+    
+#//     @singledispatchmethod
+#//     def __getitem__(self,pos:int|slice|tuple[int|slice,int|slice]):
+#//         raise TypeError
+#//     @__getitem__.register
+#//     def _(self,pos:int):
+#//         return self.data[pos]
+#//     @__getitem__.register
+#//     def _(self,pos:slice):
+#//         if not all(tuple(isinstance(element,(int,NoneType)) for element in (pos.start,pos.stop))):
+#//             raise TypeError
+#//         else:
+#//             return self.data[pos]
+#//     @__getitem__.register
+#//     def _(self,pos:tuple[int|slice]):
+#//         if not all(tuple(isinstance(axis,(int,slice)) for axis in pos)):
+#//             raise TypeError
+#//         else:
+#//             for element in pos:
+#//                 if isinstance(element,slice) and not all((isinstance(element.start,(int,NoneType)),isinstance(element.stop,(int,NoneType)))):
+#//                     raise TypeError
+#//         return self.data[pos]
+    
+#//     def __setitem__(self,index,value):
+#//         self.__getitem__(index)
+#//         self.data[index]=value
